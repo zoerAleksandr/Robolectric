2bc91813e8a9219c13a21de32e5f5b5c3759a385
zoer <zoer.aleksandr@gmail.com>
2022-07-14 09:12:38 +0300

    Написал Unit тесты на DetailsPresenter.kt

--- app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
@@ -6,7 +6,7 @@ import com.geekbrains.tests.view.details.ViewDetailsContract
 internal class DetailsPresenter internal constructor(
     private var count: Int = 0
 ) : PresenterDetailsContract {
-    private var viewContract: ViewDetailsContract? = null
+    var viewContract: ViewDetailsContract? = null
 
     override fun onAttach(v: ViewContract) {
         if (v is ViewDetailsContract) {
